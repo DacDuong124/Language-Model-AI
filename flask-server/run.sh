fb3f978d3e7d901cfb1e18e7a13acaa4ec876adf
Brian <hoangdesu@gmail.com>
2024-01-19 17:28:38 +0700

    feat: update run script for server

---
+++ flask-server/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# echo "export $PWD/language-ai-model-firebase-adminsdk-l4hgq-1c59e87bd8.json"
+export GOOGLE_APPLICATION_CREDENTIALS="$PWD/language-ai-model-firebase-adminsdk-l4hgq-1c59e87bd8.json"
+
+python3 server.py
